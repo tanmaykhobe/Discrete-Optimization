3729ddbf81afb85935b46c80107938b12e68ef46
tanmaykhobe <136801703+tanmaykhobe@users.noreply.github.com>
2023-07-04 18:39:31 +0530

    Update solver_function.py

--- facility/solver_function.py
+++ facility/solver_function.py
@@ -9,51 +9,55 @@ def length(point1, point2):
 
 
 def solver_using_scip(facilities, customers):
+    
     no_f = len(facilities)
     no_c = len(customers)
 
-
     # Create the mip solver with the SCIP backend.
     solver = pywraplp.Solver.CreateSolver('SCIP')
+
+    # Set time limit to 30 mins and if number of facilities is greater than 500, set time limit to 90 mins
     time_limit = 1800
     if no_f >= 500:
         time_limit *= 3
     solver.SetTimeLimit(int(time_limit * 1000))
 
     # DECISION VARIABLES
+    # The decision variables are - for each facility, a boolean variable that denotes the status of facility (open/closed)
+    # and other (number of facilities * number of customers) variables that denote if facility f is serving customer c
+    # for all f belonging to set of Facilities and c belonging to set of Customers
     open = {}
-    x = {}
+    x    = {}
     for i in range(no_f):
-        open[i] = solver.BoolVar(f'open{i}')
+        open[i] = solver.BoolVar(f'is facility {i} open')
         for j in range(no_c):
-            x[i,j] = solver.BoolVar(f'x{i}{j}')
-
-
-    # print('Number of variables =', solver.NumVariables())
-    
+            x[i,j] = solver.BoolVar(f'does facility {i} serve customer {j}')
 
 
     #CONSTRAINTS
+    
+    # Each customer should be served by exactly one facility
     for i in range(no_c):
         tempsum = 0
         tempsum += solver.Sum(x[j,i] for j in range(no_f))
         solver.Add(tempsum == 1)
 
+    # The sum of demands of customers served by a particular facility should be less than maximum capacity of the facility
     for i in range(no_f):
         sumOfDemands = 0
         sumOfDemands += solver.Sum(customers[j].demand * x[i,j] for j in range(no_c))
         solver.Add(sumOfDemands <= facilities[i].capacity)
-    
+
+    # Facility can only serve a customer if its open
     for i in range(no_f):
         sumrow = 0
         for j in range(no_c):
             sumrow += x[i,j]
         solver.Add(sumrow <= no_c * open[i])
             
-        
-    # print('Number of constraints =', solver.NumConstraints())
-            
+
     # OBJECTIVE
+    # The objective is minimizing the setup cost of facilities and minimizing the sum of distances between customers and facilities
     sumSetup = 0
     for i in range(no_f):
         sumSetup += open[i] * facilities[i].setup_cost 
@@ -84,5 +88,5 @@ def solver_using_scip(facilities, customers):
         for j in range(no_c): 
             if x[i,j].solution_value() == 1 :
                 res[j] = i
-    
+    # return the list of facility indices that serve the respective customers in the set of all customers
     return res
