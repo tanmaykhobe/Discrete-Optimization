8e466eb5b91004fdceb68e8ab91e4946fc23174c
tanmaykhobe <136801703+tanmaykhobe@users.noreply.github.com>
2023-07-04 17:39:35 +0530

    Update and rename solution1.py to solution_using_solver.py

--- setcover/solution_using_solver.py
+++ setcover/solution_using_solver.py
@@ -1,23 +1,29 @@
 from ortools.linear_solver import pywraplp
 
 def myfun(noItems, noSets, sets):
-    # print(sets)
 
     solver = pywraplp.Solver.CreateSolver('SCIP')
     timeL = 1800
     solver.SetTimeLimit(timeL * 1000)
-    # Decision variables
+
+    
+    # DECISION VARIABLES
     setStatus = {}
+    
     for i in range(noSets):
         setStatus[i] = solver.BoolVar(f'status{i}')
 
-    # Objective
+    
+    # OBJECTIVE FUNCTION
     cost = 0
+    
     for i in range(noSets):
         cost += sets[i].cost * setStatus[i]
+        
     solver.Minimize(cost)
 
-    # Constraints
+    
+    # CONSTRAINTS
     for j in range(noItems):
         itemSum = 0
         for i in range(noSets):
@@ -26,6 +32,8 @@ def myfun(noItems, noSets, sets):
                     itemSum += setStatus[i]
         solver.Add(itemSum >= 1)
 
+    
+    # SOLVE MODEL
     solver.Solve()
 
     res = []
