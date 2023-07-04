ab89c76bbeb8cdd4d2ccbca954ea96e1b30f3eba
tanmaykhobe <136801703+tanmaykhobe@users.noreply.github.com>
2023-07-04 17:40:23 +0530

    Update solver.py

--- setcover/solver.py
+++ setcover/solver.py
@@ -1,31 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# The MIT License (MIT)
-#
-# Copyright (c) 2014 Carleton Coffrin
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-
 
 from collections import namedtuple
-import solution1 as sol1
+import solution_using_solver as sol
 
 Set = namedtuple("Set", ['index', 'cost', 'items'])
 
@@ -44,17 +22,8 @@ def solve_it(input_data):
         parts = lines[i].split()
         sets.append(Set(i-1, int(parts[0]), [int(j) for j in parts[1:]]))
 
-    # build a trivial solution
-    # pick add sets one-by-one until all the items are covered
-    # solution = [0]*set_count
-    # coverted = set()
-    
-    # for s in sets:
-    #     solution[s.index] = 1
-    #     coverted |= set(s.items)
-    #     if len(coverted) >= item_count:
-    #         break
-    solution = sol1.myfun(item_count, set_count, sets)
+
+    solution = sol.myfun(item_count, set_count, sets)
         
     # calculate the cost of the solution
     obj = sum([s.cost*solution[s.index] for s in sets])
